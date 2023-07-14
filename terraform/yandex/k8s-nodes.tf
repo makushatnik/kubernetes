09874eb58cb01d03d8708d56de4cc1f180c4c764
Evgeny Ageev <makushatnik@gmail.com>
2023-07-14 13:33:00 -0700

    Added Bastion Host, screens

--- terraform/yandex/k8s-nodes.tf
+++ terraform/yandex/k8s-nodes.tf
@@ -1,39 +1,3 @@
-# Network
-
-resource "yandex_vpc_network" "k8s-network" {
-  name = "k8s-network"
-}
-
-resource "yandex_vpc_subnet" "k8s-subnet-1" {
-  name           = "k8s-subnet-1"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.k8s-network.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
-  depends_on = [
-    yandex_vpc_network.k8s-network,
-  ]
-}
-
-resource "yandex_vpc_subnet" "k8s-subnet-2" {
-  name           = "k8s-subnet-2"
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.k8s-network.id
-  v4_cidr_blocks = ["192.168.20.0/24"]
-  depends_on = [
-    yandex_vpc_network.k8s-network,
-  ]
-}
-
-resource "yandex_vpc_subnet" "k8s-subnet-3" {
-  name           = "k8s-subnet-3"
-  zone           = "ru-central1-c"
-  network_id     = yandex_vpc_network.k8s-network.id
-  v4_cidr_blocks = ["192.168.30.0/24"]
-  depends_on = [
-    yandex_vpc_network.k8s-network,
-  ]
-}
-
 # Compute instance group for control-plane
 
 resource "yandex_compute_instance_group" "k8s-masters" {
@@ -68,7 +32,6 @@ resource "yandex_compute_instance_group" "k8s-masters" {
         yandex_vpc_subnet.k8s-subnet-2.id,
         yandex_vpc_subnet.k8s-subnet-3.id,
       ]
-      nat = true
     }
 
     metadata = {
@@ -138,7 +101,6 @@ resource "yandex_compute_instance_group" "k8s-workers" {
         yandex_vpc_subnet.k8s-subnet-2.id,
         yandex_vpc_subnet.k8s-subnet-3.id,
       ]
-      nat = true
     }
 
     metadata = {
@@ -208,7 +170,6 @@ resource "yandex_compute_instance_group" "k8s-haproxy" {
         yandex_vpc_subnet.k8s-subnet-2.id,
         yandex_vpc_subnet.k8s-subnet-3.id,
       ]
-      nat = true
     }
 
     metadata = {
@@ -230,6 +191,70 @@ resource "yandex_compute_instance_group" "k8s-haproxy" {
       "ru-central1-a",
       "ru-central1-b",
       "ru-central1-c",
+    ]
+  }
+
+  deploy_policy {
+    max_unavailable = 1
+    max_creating    = 1
+    max_expansion   = 1
+    max_deleting    = 1
+  }
+}
+
+# Compute instance group for bastion
+
+resource "yandex_compute_instance_group" "bastion" {
+  name               = "bastion"
+  service_account_id = var.service_account_id
+  depends_on = [
+    yandex_vpc_network.k8s-network,
+    yandex_vpc_subnet.gateway
+  ]
+
+  instance_template {
+
+    name = "bastion"
+
+    resources {
+      cores         = 2
+      memory        = 2
+      core_fraction = 5
+    }
+
+    boot_disk {
+      initialize_params {
+        image_id = "fd8vmcue7aajpmeo39kk" # Ubuntu 20.04 LTS
+        size     = 10
+        type     = "network-hdd"
+      }
+    }
+
+    network_interface {
+      network_id = yandex_vpc_network.k8s-network.id
+      subnet_ids = [
+        yandex_vpc_subnet.gateway.id,
+      ]
+      nat = true
+    }
+
+    metadata = {
+      ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+    }
+    network_settings {
+      type = "STANDARD"
+    }
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = 1
+    }
+  }
+
+  allocation_policy {
+    zones = [
+      "ru-central1-a"
     ]
   }
 
