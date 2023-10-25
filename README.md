2f39c8a478ea6df1538279f8cde3495d0a8274dd
Evgeny Ageev <eageev.javaee@gmail.com>
2023-10-25 11:23:58 -0700

    Ingress Nginx controller changed to NodePort

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# kubernetes
+# E-Shop on Kubernetes
+
+## Deploy
+1. Create an account on Yandex Cloud (if you don't have one).
+2. Getting YCloud token:
+`yc iam create-token`
+3. Write this token to terraform/yandex/vars.tf at the yc_token variable.
+4. Creating servers as much as needed:
+`terraform apply`
+5. Take IPs printed after previous command was done & write them at the hosts.ini file.
+6. Setup servers:
+`ansible-playbook -i hosts.ini kube-ansible.yml`
+7. Setup Kubernetes:
+```
+kubeadm init --control-plane-endpoint ... --upload-certs --pod-network-cidr 192.168.0.0/16 | tee -a kubeadm.log
+kubeadm join ...:6443 --token ... --discovery-token-ca-cert-hash sha256:... --control-plane --certificate-key
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/baremetal/deploy.yaml
+```
+
+
--- terraform/yandex/k8s-nodes.tf
+++ terraform/yandex/k8s-nodes.tf
@@ -32,6 +32,7 @@ resource "yandex_compute_instance_group" "k8s-masters" {
         yandex_vpc_subnet.k8s-subnet-2.id,
         yandex_vpc_subnet.k8s-subnet-3.id,
       ]
+      nat = true
     }
 
     metadata = {
@@ -101,6 +102,7 @@ resource "yandex_compute_instance_group" "k8s-workers" {
         yandex_vpc_subnet.k8s-subnet-2.id,
         yandex_vpc_subnet.k8s-subnet-3.id,
       ]
+      nat = true
     }
 
     metadata = {
@@ -170,6 +172,7 @@ resource "yandex_compute_instance_group" "k8s-haproxy" {
         yandex_vpc_subnet.k8s-subnet-2.id,
         yandex_vpc_subnet.k8s-subnet-3.id,
       ]
+      nat = true
     }
 
     metadata = {
@@ -204,64 +207,64 @@ resource "yandex_compute_instance_group" "k8s-haproxy" {
 
 # Compute instance group for bastion
 
-resource "yandex_compute_instance_group" "bastion" {
-  name               = "bastion"
-  service_account_id = var.service_account_id
-  depends_on = [
-    yandex_vpc_network.k8s-network,
-    yandex_vpc_subnet.gateway
-  ]
+# resource "yandex_compute_instance_group" "bastion" {
+#   name               = "bastion"
+#   service_account_id = var.service_account_id
+#   depends_on = [
+#     yandex_vpc_network.k8s-network,
+#     yandex_vpc_subnet.gateway
+#   ]
 
-  instance_template {
+#   instance_template {
 
-    name = "bastion"
+#     name = "bastion"
 
-    resources {
-      cores         = 2
-      memory        = 2
-      core_fraction = 5
-    }
+#     resources {
+#       cores         = 2
+#       memory        = 2
+#       core_fraction = 5
+#     }
 
-    boot_disk {
-      initialize_params {
-        image_id = "fd8vmcue7aajpmeo39kk" # Ubuntu 20.04 LTS
-        size     = 10
-        type     = "network-hdd"
-      }
-    }
+#     boot_disk {
+#       initialize_params {
+#         image_id = "fd8vmcue7aajpmeo39kk" # Ubuntu 20.04 LTS
+#         size     = 10
+#         type     = "network-hdd"
+#       }
+#     }
 
-    network_interface {
-      network_id = yandex_vpc_network.k8s-network.id
-      subnet_ids = [
-        yandex_vpc_subnet.gateway.id,
-      ]
-      nat = true
-    }
+#     network_interface {
+#       network_id = yandex_vpc_network.k8s-network.id
+#       subnet_ids = [
+#         yandex_vpc_subnet.gateway.id,
+#       ]
+#       nat = true
+#     }
 
-    metadata = {
-      ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-    }
-    network_settings {
-      type = "STANDARD"
-    }
-  }
+#     metadata = {
+#       ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+#     }
+#     network_settings {
+#       type = "STANDARD"
+#     }
+#   }
 
-  scale_policy {
-    fixed_scale {
-      size = 1
-    }
-  }
+#   scale_policy {
+#     fixed_scale {
+#       size = 1
+#     }
+#   }
 
-  allocation_policy {
-    zones = [
-      "ru-central1-a"
-    ]
-  }
+#   allocation_policy {
+#     zones = [
+#       "ru-central1-a"
+#     ]
+#   }
 
-  deploy_policy {
-    max_unavailable = 1
-    max_creating    = 1
-    max_expansion   = 1
-    max_deleting    = 1
-  }
-}
+#   deploy_policy {
+#     max_unavailable = 1
+#     max_creating    = 1
+#     max_expansion   = 1
+#     max_deleting    = 1
+#   }
+# }
